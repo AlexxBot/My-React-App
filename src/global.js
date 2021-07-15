de8e273d680ab022686d6b781e0ce1f7c10a85d3
Alex Ttito <alexx_96_sf17@hotmail.com>
2021-07-15 12:05:49 -0500

    cambio de url de deployment

--- src/global.js
+++ src/global.js
@@ -1,10 +1,10 @@
 //export const URL =  'http://192.168.100.59:3000'
 
-/* export const URL = "https://api-rest-auth-node.herokuapp.com"
-export const URL_SOCKET = "https://api-rest-auth-node.herokuapp.com" */
+export const URL = "https://api-rest-auth-node.herokuapp.com"
+export const URL_SOCKET = "https://api-rest-auth-node.herokuapp.com"
 
-export const URL = "http://localhost:3000"
-export const URL_SOCKET = "http://localhost:3000"
+/* export const URL = "http://localhost:3000"
+export const URL_SOCKET = "http://localhost:3000" */
 
 export const HEADERS = {
     'Accept': 'application/json',
