a0000ea963c88ebca11bba98261b59b71bba0d29
Alex Ttito <alexx_96_sf17@hotmail.com>
2021-08-27 16:37:11 -0500

    configuracion de optiones de web sockets

--- src/components/visualizar/visualizar.js
+++ src/components/visualizar/visualizar.js
@@ -7,14 +7,41 @@ import { URL_SOCKET } from '../../global'
 
 import Mensaje from './mensaje'
 
+var connectionOptions = {
+    "force new connection": true,
+    "reconnectionAttempts": "Infinity",
+    "timeout": 10000,
+    "transports": ["websocket"]
+};
 
-const socket = io.connect(URL_SOCKET)
+const socket = io.connect(URL_SOCKET, connectionOptions)
 
 const peerConfig =
 {
     'iceServers': [
+        { urls: 'stun:stun01.sipphone.com' },
+        { urls: 'stun:stun.ekiga.net' },
+        { urls: 'stun:stun.fwdnet.net' },
+        { urls: 'stun:stun.ideasip.com' },
+        { urls: 'stun:stun.iptel.org' },
+        { urls: 'stun:stun.rixtelecom.se' },
+        { urls: 'stun:stun.schlund.de' },
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
+        { urls: 'stun:stunserver.org' },
+        { urls: 'stun:stun.softjoys.com' },
+        { urls: 'stun:stun.voiparound.com' },
+        { urls: 'stun:stun.voipbuster.com' },
+        { urls: 'stun:stun.voipstunt.com' },
+        { urls: 'stun:stun.voxgratia.org' },
+        { urls: 'stun:stun.xten.com' },
         {
-            'urls': 'stun:stun.l.google.com:19302'
+            urls: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
         },
         {
             'urls': 'turn:192.158.29.39:3478?transport=udp',
